1c23cb2d3cc7da0c0570400e811918dc991f81ab
saddamalsalfi <saddamalsalfi@qau.edu.ye>
2026-08-09 06:45:09 +0300

    Fix supported versions syntax in version.php

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ $plugin->requires  = 2022041900; // Moodle 4.0
 $plugin->component = 'local_clonecategory';
 $plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->supported = [400, 404, 500, 501, 502]; // Compatible with Moodle 4.0 up to 5.2
+$plugin->supported = [400, 502]; // Compatible with Moodle 4.0 up to 5.2
